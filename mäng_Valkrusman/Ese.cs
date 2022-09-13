6487575900a2f7a787ec8759f6e70682d3404bb5
opilane <opilane@HP-CZC6297BBM>
2022-09-13 11:44:09 +0300

    Add project files.

---
+++ mäng_Valkrusman/Ese.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mäng_Valkrusman
+{
+    internal class Ese:Üksus
+    {
+        public string nimetus;
+        public int arv;
+
+      public Ese (string nimetus, int arv)
+      {
+            this.nimetus = nimetus;
+            this.arv = arv;
+      }
+      
+       public int punktideArv() { return arv; }
+       public string info() { return nimetus; }
+
+
+
+    }
+}
+
